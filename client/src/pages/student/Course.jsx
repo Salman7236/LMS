2bb8b2e0e9441fec107f35873f3428a598a90e58
salman <muhammadsalman7236@gmail.com>
2025-06-16 11:07:45 +0500

    Add courses array

--- client/src/pages/student/Course.jsx
+++ client/src/pages/student/Course.jsx
@@ -5,7 +5,7 @@ import { Badge } from "@/components/ui/badge";
 
 const Course = () => {
   return (
-    <Card className="overflow-hidden rounded-lg dark:bg-gray-800 bg-white shadow-lg hover:shadow-2xl transform hover:scale-105 transition-all duration-300">
+    <Card className="p-0 overflow-hidden rounded-lg dark:bg-gray-800 bg-white shadow-lg hover:shadow-2xl transform hover:scale-105 transition-all duration-300">
       <div className="relative">
         <img
           src="https://img-c.udemycdn.com/course/750x422/3873464_403c_3.jpg"
@@ -13,7 +13,7 @@ const Course = () => {
           className="w-full h-36 object-cover rounded-t-lg"
         />
       </div>
-      <CardContent>
+      <CardContent className="px-5 py-4 pt-3 space-y-3">
         <h1 className="hover:underline font-bold text-lg truncate">
           Next.js Complete Course
         </h1>
@@ -23,11 +23,18 @@ const Course = () => {
               <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
               <AvatarFallback>CN</AvatarFallback>
             </Avatar>
-            <h1 className="font-medium text-sm">Instructor ABC</h1>
+            <h1 className="font-medium text-sm ">Instructor</h1>
           </div>
-          <Badge>
+          <Badge
+            className={
+              "bg-blue-600 text-white px-2 py-1 text-sm rounded-full ml-2"
+            }
+          >
             Advance
           </Badge>
+        </div>
+        <div className="text-lg font-bold">
+          <span>Rs499</span>
         </div>
       </CardContent>
     </Card>
--- client/src/pages/student/Courses.jsx
+++ client/src/pages/student/Courses.jsx
@@ -2,6 +2,8 @@ import { Skeleton } from "@/components/ui/skeleton";
 import React from "react";
 import Course from "./Course.jsx";
 
+const courses = [1, 2, 3, 4, 5, 6];
+
 const Courses = () => {
   const isLoading = false;
   return (
@@ -9,13 +11,11 @@ const Courses = () => {
       <div className="max-w-7xl mx-auto p-6">
         <h2 className="font-bold text-3xl text-center mb-10">Our Courses</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-          {isLoading ? (
-            Array.from({ length: 8 }).map((_, index) => (
-              <CourseSkeleton key={index} />
-            ))
-          ) : (
-            <Course />
-          )}
+          {isLoading
+            ? Array.from({ length: 8 }).map((_, index) => (
+                <CourseSkeleton key={index} />
+              ))
+            : courses.map((course, index) => <Course key={index} />)}
         </div>
       </div>
     </div>
